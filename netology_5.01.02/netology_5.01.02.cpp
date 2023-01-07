cbf010cee3d1024d347dbebc7074c2446c923d67
maxys-home <maxys146@gmail.com>
2023-01-07 22:52:28 +0300

    Добавил функцию из задания.

--- netology_5.01.02/netology_5.01.02.cpp
+++ netology_5.01.02/netology_5.01.02.cpp
@@ -2,6 +2,33 @@
 //
 
 #include <iostream>
+#include <variant>
+#include <vector>
+
+std::variant<int, std::string, std::vector<int>> get_variant() {
+	std::srand(std::time(nullptr));
+	int random_variable = std::rand() % 3;
+
+	std::variant<int, std::string, std::vector<int>> result;
+	switch (random_variable)
+	{
+	case 0:
+		std::cout << "/n0: ";
+		result = 5;
+		break;
+	case 1:
+		std::cout << "/n1: ";
+		result = "string";
+		break;
+	case 2:
+		std::cout << "/n2: ";
+		result = std::vector<int>{ 1, 2, 3, 4, 5 };
+		break;
+	default:
+		break;
+	}
+	return result;
+}
 
 int main()
 {
