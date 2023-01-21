986e05721402b055019eb8d85d91d17dec1f2331
maxys-home <maxys146@gmail.com>
2023-01-21 22:23:14 +0300

    Добавил заготовку задания 3.2 из задания 3.1

--- netology_5.03.02/netology_5.03.02.cpp
+++ netology_5.03.02/netology_5.03.02.cpp
@@ -2,10 +2,67 @@
 //
 
 #include <iostream>
+#include <exception>
+
+class smart_array {
+public:
+    int* arr;
+    int size;
+    smart_array(const int size) {
+        this->size = size;
+        int* constructorArray = new int[size]();
+        arr = constructorArray;
+    }
+    int get_element(int number) {
+        if (number >= 0 and number < this->size)
+        {
+            return this->arr[number];
+        }
+        else
+        {
+            throw std::exception("Wrong index!");
+        }
+    }
+    void add_element(int value) {
+        for (int i = 0; i < size; i++) {
+            if (arr[i] == 0) {
+                this->arr[i] = value;
+                break;
+            }
+        }
+    }
+
+    ~smart_array() {
+        delete[] arr;
+    }
+
+};
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale(LC_ALL, "Russian");
+
+    try {
+        smart_array arr(5);
+
+        arr.add_element(1);
+        arr.add_element(4);
+        arr.add_element(155);
+        arr.add_element(14);
+        arr.add_element(15);
+        arr.add_element(54); // Проверка что 6 элемент не добавляется.
+
+        for (int i = 0; i < 6; i++) { // Проверка что срабатывает exception для 6 элемента
+            std::cout << "arr[" << i << "] = " << arr.get_element(i) << "\n";
+        }
+    }
+    catch (const std::exception& ex) {
+        std::cout << "Exception: " << ex.what() << std::endl;
+    }
+
+
+
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
